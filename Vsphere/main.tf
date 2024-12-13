4ff3e16bbb1d7e1ff7d954764765244d79151b4a
nishuri <mn.manishcandy268@gmail.com>
2024-12-13 13:37:05 +0100

    Update main.tf

--- Vsphere/main.tf
+++ Vsphere/main.tf
@@ -1,15 +1,14 @@
 module "windows_vm" {
   source          = "./windows"
-  dc              = "Your_Datacenter_Name"
-  datastore       = "Your_Datastore_Name"
-  vmrp            = "Your_Resource_Pool_Name"
-  network_cards   = "Your_Network_Name"
-  name            = "Your_VM_Name"
-  guest_id        = "Your_Guest_ID"
+  dc              = "example-ds"
+  datastore       = "example-ds"
+  vmrp            = "VMW/Resources"
+  network_cards   = "example-nw"
+  name            = "My_Test_Windows_Server"
+  guest_id        = "windows9_64Guest"
   cpu_number      = 2
   ram_size        = 4096
-  disk_label      = "Your_Disk_Label"
+  disk_label      = "Disk1"
   disk_size_gb    = 50
   thinprovisoned  = true
-  tags            = ["tag1", "tag2"]
 }
