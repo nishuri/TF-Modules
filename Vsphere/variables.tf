df465f646be6512328354af6a5c5fd4380234187
nishuri <mn.manishcandy268@gmail.com>
2024-12-13 13:41:56 +0100

    Update variables.tf

--- Vsphere/variables.tf
+++ Vsphere/variables.tf
@@ -1,1 +1,17 @@
+variable "vsphere_server" {
+  description = "vSphere server address"
+  type        = string
+  sensitive   = true
+}
 
+variable "vsphere_username" {
+  description = "vSphere username"
+  type        = string
+  sensitive   = true
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+  type        = string
+  sensitive   = true
+}
