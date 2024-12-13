955a6f3daddce9b8a90c0c8effd0b9673b77495d
nishuri <mn.manishcandy268@gmail.com>
2024-12-13 08:39:14 +0100

    Update variable.tf

--- Vsphere/variable.tf
+++ Vsphere/variable.tf
@@ -1,1 +1,59 @@
+variable "dc" {
+  description = "Name of the datacenter"
+  type        = string
+}
 
+variable "datastore" {
+  description = "Name of the datastore"
+  type        = string
+}
+
+variable "vmrp" {
+  description = "Name of the resource pool"
+  type        = string
+}
+
+variable "network_cards" {
+  description = "Name of the network"
+  type        = string
+}
+
+variable "name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "guest_id" {
+  description = "Guest ID for the virtual machine"
+  type        = string
+}
+
+variable "cpu_number" {
+  description = "Number of CPUs for the virtual machine"
+  type        = number
+}
+
+variable "ram_size" {
+  description = "RAM size for the virtual machine in MB"
+  type        = number
+}
+
+variable "disk_label" {
+  description = "Label for the disk"
+  type        = string
+}
+
+variable "disk_size_gb" {
+  description = "Disk size in GB"
+  type        = number
+}
+
+variable "thinprovisoned" {
+  description = "Whether the disk is thin provisioned"
+  type        = bool
+}
+
+variable "tags" {
+  description = "Tags for the virtual machine"
+  type        = list(string)
+}
